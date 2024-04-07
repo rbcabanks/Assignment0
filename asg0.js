02419c26ea0950cbdc42ad73c9a38255e40edb41
rbcabanks <85521283+rbcabanks@users.noreply.github.com>
2024-04-07 16:35:36 -0700

    fixed drawVector

--- asg0.js
+++ asg0.js
@@ -1,11 +1,12 @@
 // DrawTriangle.js (c) 2012 matsuda
-function main() {  
+
+function main() {
   // Retrieve <canvas> element
-  var canvas = document.getElementById('example');  
-  if (!canvas) { 
+  var canvas = document.getElementById('example');
+  if (!canvas) {
     console.log('Failed to retrieve the <canvas> element');
-    return false; 
-  } 
+    return false;
+  }
 
   // Get the rendering context for 2DCG
   var ctx = canvas.getContext('2d');
@@ -13,4 +14,14 @@ function main() {
   // Draw a blue rectangle
   ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
   ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+
+  let v1 = new Vector3(120, 10, 0);
+  drawVector(ctx,v1,'red');
+}
+
+function drawVector(canv,v,c){
+  canv.beginPath();
+  canv.moveTo(125, 20);
+  canv.lineTo(170, 120);
+  canv.stroke();
 }
